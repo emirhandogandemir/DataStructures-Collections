53a53ddd9128728a7ed8f882797b4d0586f83689
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-09-28 23:53:08 +0300

    map added

---
+++ src/dataStructuresAndCollections/List/Main.java
@@ -0,0 +1,24 @@
+package dataStructuresAndCollections.List;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Array List
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        ListIterator listIterator = arrayList.listIterator();
+        // ArrayListi baştan sona doğru tarıyoruz
+        while (listIterator.hasNext()){
+            System.out.println(listIterator.nextIndex()+".ncı eleman");
+            System.out.println(listIterator.next());
+        }*/
+
+
+    }
+}
---
+++ src/dataStructuresAndCollections/map/Main.java
@@ -0,0 +1,12 @@
+package dataStructuresAndCollections.map;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap hash = new HashMap();
+        hash.put("bir",1);
+        hash.put("iki",2);
+        hash.put("üç",3);
+    }
+}
